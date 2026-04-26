df7cad0218e7aa657f489c9a0c2e39fc647cb53d
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-04-26 18:46:00 -0400

    create users in your database

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,3 +4,19 @@ import { prisma } from "../lib/prisma.js";
 export async function signUpGet(req, res) {
     res.render('sign-up',  { errors: [] });
 };
+
+export async function createUser(req, res, next) {
+     try {
+        await prisma.user.create({
+            data: {
+                name: req.body.name,
+                username: req.body.username, 
+                password: req.body.password
+            }
+        });
+        res.redirect("/");
+     } catch (err) {
+        return next(err);
+     }
+};
+
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
   import express from "express";
  import { Router } from "express";
- import { signUpGet } from "../controllers/auth.js";
+ import { signUpGet, createUser } from "../controllers/auth.js";
 
 
 const router = Router();
 
 router.get('/signup', signUpGet); 
+router.post('/signup', createUser)
 
 
 export { router };
