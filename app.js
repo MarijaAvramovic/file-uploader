3fc4ab01651a9204ea8d43806f4a8ff88b46a928
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-04-26 17:50:07 -0400

    add express-session and passport

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";import express from "express";
+import { prisma } from './lib/prisma.js'
+import session from "express-session";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+ 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({
+  secret: "cats",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
