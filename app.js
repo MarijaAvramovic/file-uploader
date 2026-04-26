e2a01e1982bdde558fe5180035350e5773510eca
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-04-26 18:34:46 -0400

    style sign-up form

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 
 app.use(session({
   secret: "cats",
